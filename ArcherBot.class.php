904d2520fc0ea8c431c47b7522887f9b78eb17de
Richard Clifford <rclifford@cybershade.org>
2014-09-25 13:06:37 +0100

    MOAR

---
+++ ArcherBot.class.php
@@ -0,0 +1,100 @@
+<?php namespace Pentest\ArcherBot;
+
+require_once './vendor/autoload.php';
+
+class ArcherBot {
+
+    protected $objGoutte;
+    protected $strTarget    = '';
+    protected $strUserAgent = '';
+    protected $arrChecks    = array();
+    protected $arrFindings  = array();
+
+    public function __construct($strTarget) {
+
+        $this->objGoutte = new \Goutte\Client();
+
+        $this->arrChecks = include('./config.php');
+
+        // Initally set the target
+        $this->strTarget = (string)$strTarget;
+
+        $this->parseOutFiles();
+    }
+
+    public function setTarget($strTarget) {
+
+        if(!is_string($strTarget) || empty($strTarget)) {
+
+            throw new \Exception('Failed to correctly set target - Ensure target is a valid string');
+        }
+
+        return ($this->strTarget = $strTarget);
+    }
+
+    public function parseOutFiles() {
+
+        $objCrawler = $this->objGoutte->request('GET', $this->strTarget);
+
+        $objCrawler->filter('script')->each(function($objNode){
+
+            $mxdSrc = $objNode->attr('src');
+
+            if(is_null($mxdSrc)) {
+
+                // continue;
+                return false;
+            }
+
+            // Make a copy
+            $strUrl = $mxdSrc;
+
+            // Check if it's relative or absolute path
+            if((substr($strUrl, 0, 7) !== 'http://') && (substr($strUrl, 0, 8) !== 'https://') && (substr($strUrl, 0, 2) !== '//')) {
+
+                $strUrl = $this->strTarget . '/' . $mxdSrc;
+
+            } elseif((substr($strUrl, 0, 2) === '//')) {
+
+                $strUrl = 'http:' . $strUrl;
+            }
+
+            $mxdJavascriptFile = file_get_contents($strUrl);
+
+            if(false !== $mxdJavascriptFile) {
+
+                foreach($this->arrChecks as $strKey => $arrCheck){
+
+                    if(is_array($arrCheck) && !empty($arrCheck)) {
+
+                        foreach($arrCheck as $strCheck) {
+
+                            if(false !== stristr($mxdJavascriptFile, $strCheck)) {
+
+                                $this->arrFindings[$strKey]['url'] = $strUrl;
+                                $this->arrFindings[$strKey]['vulnerability'] = $strCheck;
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    public function generateReport() {
+
+        $strOut = '';
+
+        if(empty($this->arrFindings)) {
+
+            $strOut = 'Sorry, we couldn\'t find anything in the target specified.';
+        } else {
+
+            foreach($this->arrFindings as $strKey => $arrValues) {
+                $strOut .= sprintf("Case: %s has %d vulnerabilities:\r\n%s", $strKey, count($arrValues), print_r($arrValues, true));
+            }
+        }
+
+        return $strOut;
+    }
+}
